2db8c9d267a6617ed06ee3b697dde39fe807d1f2
Saadgi Puniwala <saadgipuniwala07@gmail.com>
2026-06-30 18:21:05 +0530

    Time: 9 ms (5.65%), Space: 10.7 MB (5.43%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,54 @@
+class Solution {
+private:
+    bool valid(char ch){
+        if((ch >= 'a' && ch <= 'z') ||
+           (ch >= 'A' && ch <= 'Z') ||
+           (ch >= '0' && ch <= '9')){
+            return true;
+        }
+        return false;
+    }
+
+    char tolowercase(char ch){
+        if((ch >= 'a' && ch <= 'z') ||
+           (ch >= '0' && ch <= '9')){
+            return ch;
+        }
+        else{
+            return ch - 'A' + 'a';
+        }
+    }
+
+    bool checkpallindrome(string a){
+        int s = 0;
+        int e = a.length() - 1;
+
+        while(s <= e){
+            if(a[s] != a[e]){
+                return false;
+            }
+            s++;
+            e--;
+        }
+
+        return true;
+    }
+
+public:
+    bool isPalindrome(string s) {
+
+        string temp = "";
+
+        for(int j = 0; j < s.length(); j++){
+            if(valid(s[j])){
+                temp.push_back(s[j]);
+            }
+        }
+
+        for(int j = 0; j < temp.length(); j++){
+            temp[j] = tolowercase(temp[j]);
+        }
+
+        return checkpallindrome(temp);
+    }
+};
