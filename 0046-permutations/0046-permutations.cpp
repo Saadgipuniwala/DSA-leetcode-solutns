8289e2feaeb931553abe444f833b5a934057d5b3
Saadgi Puniwala <saadgipuniwala07@gmail.com>
2026-07-20 20:36:05 +0530

    Time: 0 ms (100%), Space: 10.5 MB (89%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    void solve(vector<int>& nums, vector<vector<int>>& ans, int index) {
+
+        // Base case
+        if (index >= nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int j = index; j < nums.size(); j++) {
+
+            // Choose
+            swap(nums[index], nums[j]);
+
+            // Explore
+            solve(nums, ans, index + 1);
+
+            // Backtrack
+            swap(nums[index], nums[j]);
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+        int index = 0;
+
+        solve(nums, ans, index);
+
+        return ans;
+    }
+};
