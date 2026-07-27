4d5fce12962da8dca7d03d8421e549e85df8c1b7
Saadgi Puniwala <saadgipuniwala07@gmail.com>
2026-07-27 11:11:49 +0530

    Time: 0 ms (100%), Space: 117.9 MB (82.31%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,7 +10,55 @@
  */
 class Solution {
 public:
+     ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
     bool isPalindrome(ListNode* head) {
+
+        if (head == NULL || head->next == NULL)
+            return true;
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next != NULL && fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* secondHalf = reverse(slow->next);
+
+        // Compare both halves
+        ListNode* firstHalf = head;
+        ListNode* temp = secondHalf;
+
+        while (temp != NULL) {
+            if (firstHalf->val != temp->val)
+                return false;
+
+            firstHalf = firstHalf->next;
+            temp = temp->next;
+        }
+
+        return true;
+    }
+};
+
+
+    /*bool isPalindrome(ListNode* head) {     // tc:O(n)  SC: O(n)
 
         vector<int> arr;
 
@@ -34,4 +82,4 @@ public:
 
         return true;
     }
-};
+};*/
