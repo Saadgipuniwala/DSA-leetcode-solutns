ab5a5e9e4ef42c86ab02738b07ecd1a03bdd2065
Saadgi Puniwala <saadgipuniwala07@gmail.com>
2026-07-27 11:39:31 +0530

    Time: 4 ms (34.17%), Space: 77.1 MB (49.57%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -8,6 +8,113 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+class Solution {
+private:
+
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    void insertAtTail(ListNode* &head, ListNode* &tail, int val) {
+
+        ListNode* temp = new ListNode(val);
+
+        if (head == NULL) {
+            head = temp;
+            tail = temp;
+        }
+        else {
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    ListNode* add(ListNode* first, ListNode* second) {
+
+        int carry = 0;
+
+        ListNode* ansHead = NULL;
+        ListNode* ansTail = NULL;
+
+        while (first != NULL && second != NULL) {
+
+            int sum = carry + first->val + second->val;
+            int digit = sum % 10;
+
+            insertAtTail(ansHead, ansTail, digit);
+
+            carry = sum / 10;
+
+            first = first->next;
+            second = second->next;
+        }
+
+        while (first != NULL) {
+
+            int sum = carry + first->val;
+            int digit = sum % 10;
+
+            insertAtTail(ansHead, ansTail, digit);
+
+            carry = sum / 10;
+
+            first = first->next;
+        }
+
+        while (second != NULL) {
+
+            int sum = carry + second->val;
+            int digit = sum % 10;
+
+            insertAtTail(ansHead, ansTail, digit);
+
+            carry = sum / 10;
+
+            second = second->next;
+        }
+
+        while (carry != 0) {
+
+            int sum = carry;
+            int digit = sum % 10;
+
+            insertAtTail(ansHead, ansTail, digit);
+
+            carry = sum / 10;
+        }
+
+        return ansHead;
+    }
+
+public:
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        // Step 1: Reverse input lists
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        // Step 2: Add reversed lists
+        ListNode* ans = add(l1, l2);
+
+        // Step 3: Reverse answer
+        ans = reverse(ans);
+
+        return ans;
+    }
+};
+/*
 class Solution {                           //Time Complexity: O(n + m) Space Complexity: O(1)
 public:
 ListNode* reverseList(ListNode* head) {
@@ -65,4 +172,4 @@ ListNode* add(ListNode* l1, ListNode* l2) {
 
         return ans;
     }
-};
+};*/
