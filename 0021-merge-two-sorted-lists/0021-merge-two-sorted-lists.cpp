ed99d2eac65f6fc63a6ebce2b937b868327a63a5
Saadgi Puniwala <saadgipuniwala07@gmail.com>
2026-07-27 09:51:56 +0530

    Time: 0 ms (100%), Space: 19.5 MB (63.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,8 +10,8 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
+    /* ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    
         ListNode dummy(-1);
         ListNode* tail = &dummy;
 
@@ -35,5 +35,66 @@ public:
             tail->next = list2;
 
         return dummy.next;
+        }
+}; */
+
+    void solve(ListNode* first, ListNode* second) {
+
+    ListNode* curr1 = first;
+    ListNode* next1 = curr1->next;
+
+    ListNode* curr2 = second;
+    ListNode* next2 = NULL;
+
+    // Handle single-node first list
+    if (next1 == NULL) {
+        curr1->next = curr2;
+        return;
+    }
+
+    while (next1 != NULL && curr2 != NULL) {
+
+            if ((curr2->val >= curr1->val) &&
+                (curr2->val <= next1->val)) {
+
+                // Insert curr2 between curr1 and next1
+                next2 = curr2->next;
+                curr1->next = curr2;
+                curr2->next = next1;
+
+                // Move pointers
+                curr1 = curr2;
+                curr2 = next2;
+            }
+            else {
+
+                curr1 = next1;
+                next1 = next1->next;
+
+                if (next1 == NULL) {
+                    curr1->next = curr2;
+                    return;
+                }
+            }
+        }
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        if (list1 == NULL)
+            return list2;
+
+        if (list2 == NULL)
+            return list1;
+
+        // Make sure list1 starts with the smaller element
+        if (list1->val <= list2->val) {
+            solve(list1, list2);
+            return list1;
+        }
+        else {
+            solve(list2, list1);
+            return list2;
+        }
     }
 };
