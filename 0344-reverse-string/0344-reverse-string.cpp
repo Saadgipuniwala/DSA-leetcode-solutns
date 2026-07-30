06cf8462113f991d408a134c1b24be8533115563
Saadgi Puniwala <saadgipuniwala07@gmail.com>
2026-07-30 10:16:50 +0530

    Time: 8 ms (1.95%), Space: 28.2 MB (5.29%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     void reverseString(vector<char>& s) {
         int st=0, e=s.size()-1;
@@ -6,5 +6,22 @@ public:
             swap(s[st++],s[e--]);
         }
 
+    }
+};*/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        stack<char> st;
+
+        // Push all characters into the stack
+        for (char ch : s) {
+            st.push(ch);
+        }
+
+        // Pop back into the vector
+        for (int i = 0; i < s.size(); i++) {
+            s[i] = st.top();
+            st.pop();
+        }
     }
 };
